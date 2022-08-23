a734209810690f0d87729474632f3f5b2d752d71
Vinicius Silva <79378773+viniciusgdr@users.noreply.github.com>
2022-08-22 23:56:25 -0300

    docs: add example

---
+++ exemplo/example.ts
@@ -0,0 +1,15 @@
+(async () => {
+    let vizer = new Vizer()
+    let search = await vizer.search({ query: "Velozes e Furiosos", type: 'movie' })
+    console.log(search)
+    let info = await vizer.getInfo({ url: search[0].url })
+    console.log(info)
+    let player = await vizer.getPlayer({ url: search[0].url, imdbTT: info.imdbTT, language: 'pt' })
+    console.log(player)
+    
+    let download = await vizer.getDownload({
+        id: player.id,
+        player: player.players[0].split('=')[2]
+    })
+    console.log(download)
+})();
